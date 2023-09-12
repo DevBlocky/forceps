1a73b851ae6a9fdc40a69ba0150234f6a90cb9f5
Luke Seelenbinder <me@lukeseelenbinder.com>
2023-09-12 12:57:24 +0200

    Use metadata to reduce cost of misses.

--- src/cache.rs
+++ src/cache.rs
@@ -180,6 +180,10 @@ impl Cache {
             return self.track_access_for(k).map(|_| val);
         }
 
+        // read the metadata to reduce miss cost, since the metadata DB should generally fit in
+        // memory (and also removes the need to read file metadata for a hit.)
+        let Ok(meta) = self.meta.get_metadata(k) else { return Err(ForcepError::NotFound) };
+
         let file = {
             let path = self.path_from_key(k);
             afs::OpenOptions::new()
@@ -193,8 +197,7 @@ impl Cache {
         };
 
         // create a new buffer based on the estimated size of the file
-        let size_guess = file.metadata().await.map(|x| x.len()).unwrap_or(0);
-        let mut buf = Vec::with_capacity(size_guess as usize);
+        let mut buf = Vec::with_capacity(meta.get_size() as _);
 
         // read the entire file to the buffer
         tokio::io::BufReader::with_capacity(self.opts.rbuff_sz, file)
